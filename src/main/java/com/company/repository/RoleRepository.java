7e5bc6b51bf52eae369b27422934509472c55a89
Yegor S <syegorv@gmail.com>
2023-01-17 00:28:19 +0700

    add .

--- src/main/java/com/company/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.company.repository;
-
-import com.company.models.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
